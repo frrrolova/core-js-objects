7e5097201be79834d6b565755dcf431bc136ca2c
t_frrolova <tatianaaleks.frolova@gmail.com>
2024-02-28 10:12:31 +0100

    feat: makeWord implemented

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -130,8 +130,17 @@ function makeImmutable(obj) {
  *    makeWord({ a: [0, 1], b: [2, 3], c: [4, 5] }) => 'aabbcc'
  *    makeWord({ H:[0], e: [1], l: [2, 3, 8], o: [4, 6], W:[5], r:[7], d:[9]}) => 'HelloWorld'
  */
-function makeWord(/* lettersObject */) {
-  throw new Error('Not implemented');
+function makeWord(lettersObject) {
+  const map = new Map();
+  let res = '';
+  Object.entries(lettersObject).forEach(([key, val]) => {
+    val.forEach((item) => map.set(item, key));
+  });
+  for (let i = 0; i < map.size; i += 1) {
+    res += map.get(i);
+  }
+
+  return res;
 }
 
 /**
